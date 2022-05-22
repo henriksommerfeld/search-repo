017113187ce0ff8225d4400351a725db45e24ead
Henrik Sommerfeld <henrik@sommerfeld.nu>
2022-05-22 11:20:38 +0200

    Add fallback for open menu option

--- search.mjs
+++ search.mjs
@@ -1,30 +1,34 @@
 #!/usr/bin/env zx
-import { chalk } from 'zx';
-import { filePath } from './shared.mjs';
+import { filePath } from "./shared.mjs";
 
 $.verbose = false;
 const repo = argv._[1];
 if (!repo) {
   process.exit();
 }
-console.log('What do you want to do?');
-console.log(` (${chalk.blue('b')}) Browse repo on github.com `);
-console.log(` (${chalk.blue('c')}) Clone (using ssh)`);
-console.log(` (${chalk.blue('n')}) Nothing`);
+console.log("What do you want to do?");
+console.log(` (${chalk.blue("b")}) Browse repo`);
+console.log(` (${chalk.blue("c")}) Clone (using ssh)`);
+console.log(` (${chalk.blue("n")}) Nothing`);
 let choice = await question(
-  `(${chalk.blue('b')}/${chalk.blue('c')}/${chalk.blue('N')}) `
+  `(${chalk.blue("b")}/${chalk.blue("c")}/${chalk.blue("N")}) `,
 );
-if (['b', 'B'].includes(choice)) {
-  const htmlUrl = await getRepoProperty('html_url').then(
-    x => new URL(x.stdout)
+if (["b", "B"].includes(choice)) {
+  const htmlUrl = await getRepoProperty("html_url").then(
+    (x) => new URL(x.stdout),
   );
-  await $`open ${htmlUrl.href}`;
+  try {
+    await $`open ${htmlUrl.href}`;
+  } catch (err) {
+    console.log(htmlUrl.href);
+  }
 }
-if (['c', 'C'].includes(choice)) {
-  const sshUrl = await getRepoProperty('ssh_url').then(x => x.stdout.trim());
+if (["c", "C"].includes(choice)) {
+  const sshUrl = await getRepoProperty("ssh_url").then((x) => x.stdout.trim());
   await $`git clone ${sshUrl}`;
 }
 
 async function getRepoProperty(prop) {
-  return await $`jq '.[] | select(.name == \"${repo}\") | .${prop}'< ${filePath} | tr -d '"'`;
+  return await $
+    `jq '.[] | select(.name == \"${repo}\") | .${prop}'< ${filePath} | tr -d '"'`;
 }
